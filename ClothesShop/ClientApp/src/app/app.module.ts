58ff23cc08625f9a3bac0a0a0a716b34f0d6bf7e
79270 <79270@LAPTOP-U09DQI2H>
2023-05-03 22:23:56 +0400

    добавлены основные компоненты для страниц

--- ClothesShop/ClientApp/src/app/app.module.ts
+++ ClothesShop/ClientApp/src/app/app.module.ts
@@ -5,19 +5,45 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductListPageComponent } from './product-list-page/product-list-page.component';
+import { BasketPageComponent } from './basket-page/basket-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { ProductLikePageComponent } from './product-like-page/product-like-page.component';
+import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
+import { MainAdminPageComponent } from './main-admin-page/main-admin-page.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    ProductListPageComponent,
+    BasketPageComponent,
+    OrderPageComponent,
+    ProductLikePageComponent,
+    AuthenticationPageComponent,
+    MainAdminPageComponent,
+    NavigationComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: MainPageComponent },
+      { path: 'product-list', component: ProductListPageComponent },
+      { path: 'product/:id', component: ProductPageComponent },
+      { path: 'basket', component: BasketPageComponent },
+      { path: 'order', component: OrderPageComponent },
+      { path: 'product-like', component: ProductLikePageComponent },
+      { path: 'authentication', component: AuthenticationPageComponent },
+      { path: 'admin', component: MainAdminPageComponent },
+      { path: '**', component: ErrorPageComponent },
     ])
   ],
   providers: [],
---
+++ ClothesShop/ClientApp/src/app/pages/product-like/product-like-page/product-like-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-like-page',
+  templateUrl: './product-like-page.component.html',
+  styleUrls: ['./product-like-page.component.css']
+})
+export class ProductLikePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
