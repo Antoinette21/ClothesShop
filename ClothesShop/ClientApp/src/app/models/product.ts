e1bc13877bd0eef25b58ac6e513b84a64b7fb4f7
79270 <79270@LAPTOP-U09DQI2H>
2023-05-10 22:56:02 +0400

    add product model

---
+++ ClothesShop/ClientApp/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: string,
+  name: string,
+  size: number,
+  price: number,
+  category: string,
+  description: string,
+  image: string
+}
--- ClothesShop/ClientApp/src/app/pages/product-list/product-list-page/product-list-page.component.ts
+++ ClothesShop/ClientApp/src/app/pages/product-list/product-list-page/product-list-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../../models/product';
 
 @Component({
   selector: 'app-product-list-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListPageComponent implements OnInit {
 
+  private listProduct: Product[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public getListProduct(): Product[] {
+    return this.listProduct;
+  }
+
+  public setListProduct(products: Product[]): void {
+    this.listProduct = products;
   }
 
 }
