92299dfbd08ed568619da3ae8b2e06ef2a326de2
79270 <79270@LAPTOP-U09DQI2H>
2023-05-10 21:54:28 +0400

    Add nav icons

--- ClothesShop/ClientApp/src/app/app.module.ts
+++ ClothesShop/ClientApp/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { ProductListPageComponent } from './product-list-page/product-list-page.component';
-import { BasketPageComponent } from './basket-page/basket-page.component';
-import { OrderPageComponent } from './order-page/order-page.component';
-import { ProductLikePageComponent } from './product-like-page/product-like-page.component';
-import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
-import { MainAdminPageComponent } from './main-admin-page/main-admin-page.component';
+import { MainPageComponent } from './pages/main/main-page/main-page.component';
+import { ProductPageComponent } from './pages/product/product-page/product-page.component';
+import { ProductListPageComponent } from './pages/product-list/product-list-page/product-list-page.component';
+import { BasketPageComponent } from './pages/basket/basket-page/basket-page.component';
+import { OrderPageComponent } from './pages/order/order-page/order-page.component';
+import { ProductLikePageComponent } from './pages/product-like/product-like-page/product-like-page.component';
+import { AuthenticationPageComponent } from './pages/authentication/authentication-page/authentication-page.component';
+import { MainAdminPageComponent } from './pages/main-admin/main-admin-page/main-admin-page.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from './pages/error/error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +35,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    CarouselModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: MainPageComponent },
--- ClothesShop/ClientApp/src/app/pages/main/main-page/main-page.component.ts
+++ ClothesShop/ClientApp/src/app/pages/main/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-main-page',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
+
+  slides = [
+    { image: 'https://uxawscdn.baozun.com/aws/96001096/20230424/644AB73C352DFB640DD5CCAA8D53255F.png?x-oss-process=image/quality,q_80/format,webp', text: 'First' },
+    { image: 'https://uxawscdn.baozun.com/aws/96001096/20230424/A530434417284FF49B8A28BE5AFF7372.png?x-oss-process=image/quality,q_80/format,webp', text: 'Second' },
+    { image: 'https://uxawscdn.baozun.com/aws/96001096/20230414/4653648F2416072869E4F25EBF60BC0E.png?x-oss-process=image/quality,q_80/format,webp', text: 'Third' }
+  ];
+  noWrapSlides = false;
+  showIndicator = true;
 
   constructor() { }
 
